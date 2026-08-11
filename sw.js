09b9fad52348963ec77344fc9277b4b0d56b312d
Antimatter77843 <danny180.dp@gmail.com>
2026-08-11 18:11:45 +0100

    Add service worker

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'walkloop-v1';
+const STATIC = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(STATIC)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
+});
